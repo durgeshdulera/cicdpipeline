3a10300c4cf54dd8c847430b23d8ddecac3962bd
durgeshdulera <durgesh.dulera@gmail.com>
2025-02-23 13:23:41 +0530

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Vite uses import.meta.env to access environment variables
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/cicdpipeline/", // Use the base path from the environment variable
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "", ""); // Load env vars based on mode
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/cicdpipeline/", // Use loaded env var or default
+  };
 });
