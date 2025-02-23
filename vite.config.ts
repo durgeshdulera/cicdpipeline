a4363867f3f0977f0eef74397f541ba302ba3a87
durgeshdulera <durgesh.dulera@gmail.com>
2025-02-23 12:44:00 +0530

    environment files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Vite uses import.meta.env to access environment variables
 export default defineConfig({
   plugins: [react()],
-  base: "/cicdpipeline/",
+  base: import.meta.env.VITE_BASE_PATH || "/cicdpipeline/", // Use the base path from the environment variable
 });
